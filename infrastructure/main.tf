eaf961459a9a90e0d14fe8c23a0771beb8b24d86
markdathorne <mark.dathorne@cgi.com>
2023-03-21 14:30:56 +0000

    SDT-102 dev review changes.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,49 +1,8 @@
-provider "azurerm" {
-  features {}
-}
 locals {
   key_vault_name          = join("-", [var.product, "aat"])
-}
-
-module "db-sdt-commissioning-v11" {
-  source             = "git@github.com:hmcts/cnp-module-postgres?ref=master"
-  product            = var.product
-  component          = var.component
-  name               = join("-", [var.product, var.component, "postgres-db", "v11"])
-  location           = var.location
-  subscription       = var.subscription
-  env                = var.env
-  postgresql_user    = "sdtcomissioning"
-  database_name      = "sdtcomissioning"
-  common_tags        = var.common_tags
-  postgresql_version = "11"
 }
 
 data "azurerm_key_vault" "rd_key_vault" {
   name                = local.key_vault_name
   resource_group_name = local.key_vault_name
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-USER" {
-  name          = join("-", [var.component, "POSTGRES-USER"])
-  value         = module.db-sdt-commissioning-v11.user_name
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES-PASS" {
-  name          = join("-", [var.component, "POSTGRES-PASS"])
-  value         = module.db-sdt-commissioning-v11.postgresql_password
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_HOST" {
-  name          = join("-", [var.component, "POSTGRES-HOST"])
-  value         = module.db-sdt-commissioning-v11.host_name
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
-}
-
-resource "azurerm_key_vault_secret" "POSTGRES_DATABASE" {
-  name          = join("-", [var.component, "POSTGRES-DATABASE"])
-  value         = module.db-sdt-commissioning-v11.postgresql_database
-  key_vault_id  = data.azurerm_key_vault.rd_key_vault.id
 }
