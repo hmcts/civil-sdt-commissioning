8e5650c51fa6a92f54a629537fb1ebd00bff048e
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-05-02 15:46:17 +0100

    Removed functional test configuration

--- src/functionalTest/java/uk/gov/hmcts/reform/sdt/controllers/SampleFunctionalTest.java
+++ src/functionalTest/java/uk/gov/hmcts/reform/sdt/controllers/SampleFunctionalTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.given;
