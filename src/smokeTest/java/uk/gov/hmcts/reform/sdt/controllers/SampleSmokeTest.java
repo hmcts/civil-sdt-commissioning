7d680fa3adb92f7232e694dd66f15c31715c10ca
markdathorne <mark.dathorne@cgi.com>
2023-06-07 12:17:09 +0100

    SDT-118 adjust smoke test, provide yaml

--- src/smokeTest/java/uk/gov/hmcts/reform/sdt/controllers/SampleSmokeTest.java
+++ src/smokeTest/java/uk/gov/hmcts/reform/sdt/controllers/SampleSmokeTest.java
@@ -5,14 +5,19 @@ import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestPropertySource(properties = { "spring.config.location=classpath:application-smoke-test.yaml" })
 class SampleSmokeTest {
 
     @Value("${TEST_URL:http://localhost:8080}")
