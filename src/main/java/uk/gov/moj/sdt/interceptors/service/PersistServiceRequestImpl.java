95327968264187766f387f403d822649bb2aee82
Kiran Yenigala <kiran.yenigala@hmcts.net>
2023-07-04 20:26:29 +0100

    SDT-128: Fix the application startup issue and null pointer exception

--- src/main/java/uk/gov/moj/sdt/interceptors/service/PersistServiceRequestImpl.java
+++ src/main/java/uk/gov/moj/sdt/interceptors/service/PersistServiceRequestImpl.java
@@ -1,18 +1,28 @@
 package uk.gov.moj.sdt.interceptors.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import uk.gov.moj.sdt.domain.api.IDomainObject;
+import uk.gov.moj.sdt.producers.comx.dao.MockGenericDao;
 
 @Component
 public class PersistServiceRequestImpl implements IPersistServiceRequest {
 
+    private MockGenericDao mockGenericDao;
+
+    @Autowired
+    public PersistServiceRequestImpl(@Qualifier("GenericDao") MockGenericDao mockGenericDao) {
+        this.mockGenericDao = mockGenericDao;
+    }
+
     @Override
     public void persist(Object domainObject) {
-
+        mockGenericDao.persist(domainObject);
     }
 
     @Override
     public <D extends IDomainObject> D fetch(Class<D> domainType, long id) {
-        return null;
+        return mockGenericDao.fetch(domainType, id);
     }
 }
